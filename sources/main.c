e917a8590c163dc1c02c76be3fd92f962de74f46
ch-iv <icheppy101@gmail.com>
2023-11-18 17:13:58 -0500

    Rectangle clicker

--- sources/main.c
+++ sources/main.c
@@ -1,35 +1,101 @@
 #include "raylib.h"
 
-#define SCREEN_WIDTH (800)
-#define SCREEN_HEIGHT (450)
+#define GRID_X 4
+#define GRID_Y 4
+#define  GRID_SIZE 100
+#define  GAP_SIZE 20
+#define MAX_RECS GRID_X * GRID_Y
 
-#define WINDOW_TITLE "Window title"
+
+int clamp(int val, int max, int min) {
+    if (val > max) {
+        return max;
+    } else if (val < min) {
+        return min;
+    } else {
+        return val;
+    }
+}
+
 
 int main(void)
 {
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
-    SetTargetFPS(60);
+    SetConfigFlags(FLAG_MSAA_4X_HINT);
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 800;
+    const int screenHeight = 450;
 
-    Texture2D texture = LoadTexture(ASSETS_PATH"test.png"); // Check README.md for how this works
+    bool toggled_rectangles[MAX_RECS];
+    int total_toggled = MAX_RECS;
+    for (int i = 0; i < MAX_RECS; i++){
+        toggled_rectangles[i] = true;
+    }
 
-    while (!WindowShouldClose())
+    InitWindow(screenWidth, screenHeight, "raylib [textures] example - mouse painting");
+    MaximizeWindow();
+
+
+    while (!WindowShouldClose())    // Detect window close button or ESC key
     {
+        if (total_toggled == 0){
+            return 0;
+        }
+        // Update
+        //----------------------------------------------------------------------------------
+        int max_space = 0;
+        if (GetScreenHeight() > GetScreenWidth()){
+            max_space = GetScreenWidth();
+        } else {
+            max_space = GetScreenHeight();
+        }
+        int tot_grid_size = GRID_X * GRID_SIZE + (GRID_X - 1) * GAP_SIZE;
+        int grid_start_x = (GetScreenWidth() - tot_grid_size) / 2;
+        int grid_start_y = (GetScreenHeight() - tot_grid_size) / 2;
+        Rectangle rectangles[MAX_RECS] = {0};
+        for (int y = 0; y < GRID_Y; y++){
+            for (int x = 0; x < GRID_X; x++){
+                rectangles[y * GRID_X + x].height = GRID_SIZE;
+                rectangles[y * GRID_X + x].width = GRID_SIZE;
+                rectangles[y * GRID_X + x].x = grid_start_x + GRID_SIZE * x + x * GAP_SIZE;
+                rectangles[y * GRID_X + x].y = grid_start_y + GRID_SIZE * y + y * GAP_SIZE;
+            }
+        }
+
+        // calculating collisions
+        if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)){
+            for (int i = 0; i < MAX_RECS; i++){
+                bool colliding = CheckCollisionPointRec(GetMousePosition(), rectangles[i]);
+                if (colliding && toggled_rectangles[i]){
+                    toggled_rectangles[i] = false;
+                    total_toggled--;
+                }
+            }
+        }
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
 
-        const int texture_x = SCREEN_WIDTH / 2 - texture.width / 2;
-        const int texture_y = SCREEN_HEIGHT / 2 - texture.height / 2;
-        DrawTexture(texture, texture_x, texture_y, WHITE);
-
-        const char* text = "OMG! IT WORKS!";
-        const Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 20, 1);
-        DrawText(text, SCREEN_WIDTH / 2 - text_size.x / 2, texture_y + texture.height + text_size.y + 10, 20, BLACK);
+        // draw rectangles that are toggled
+        for (int i = 0; i < MAX_RECS; i++){
+            if (toggled_rectangles[i]) {
+                DrawRectangleRec(rectangles[i], BLACK);
+            }
+        }
 
         EndDrawing();
+        //----------------------------------------------------------------------------------
     }
 
-    CloseWindow();
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
 
     return 0;
 }
